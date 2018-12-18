ce9b5648fc496941f82c30d0c33c8143abd697cc
anneke <a.m.huijsmans@student.tue.nl>
2018-12-18 17:05:56 +0100

    added Ttree class

---
+++ TTree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anneke on 18/12/18.
+//
+
+#include "TTree.h"
---
+++ TTree.h
@@ -0,0 +1,14 @@
+//
+// Created by anneke on 18/12/18.
+//
+
+#ifndef UNTITLED_TTREE_H
+#define UNTITLED_TTREE_H
+
+
+class TTree {
+
+};
+
+
+#endif //UNTITLED_TTREE_H
