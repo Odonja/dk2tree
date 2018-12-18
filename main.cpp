41406244d65c9c047f85f97e7fb455ad716cc320
hugo <hugomelchers@gmail.com>
2018-12-18 16:36:49 +0100

    include sdsl library in cmake

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <sdsl/suffix_arrays.hpp>
+#include <fstream>
+
+using namespace sdsl;
 
 int main() {
-    std::cout << "Hello, Worldy!" << std::endl;
-    return 0;
+    csa_wt<> fm_index;
+    construct_im(fm_index, "mississippi!", 1);
+    std::cout << "'si' occurs " << count(fm_index,"si") << " times.\n";
+    store_to_file(fm_index,"fm_index-file.sdsl");
+    std::ofstream out("fm_index-file.sdsl.html");
+    write_structure<HTML_FORMAT>(fm_index,out);
 }
