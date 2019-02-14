c43aadf6ff295659fab3bcb4569fb73077a8b5e0
HugoMelchers <hugomelchers@gmail.com>
2019-02-14 17:53:55 +0100

    Declare doLargeTest() before main so that main can use it

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include "AdjacencyLists.h"
 #include "DKTree.h"
 
+void doLargeTests();
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
