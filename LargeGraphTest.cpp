c6135452e20fa890e7ca3c6919419df3c9e1dcd0
HugoMelchers <hugomelchers@gmail.com>
2019-02-15 14:21:51 +0100

    Generate X number of random edges instead of looping over all possibilities. Profiler showed that when p is small, the majority of time was spent in the double-for and not actually inserting edges

--- LargeGraphTest.cpp
+++ LargeGraphTest.cpp
@@ -11,12 +11,10 @@
 #include <thread>
 
 std::default_random_engine generator((unsigned long) std::chrono::steady_clock::now().time_since_epoch().count());
-std::uniform_real_distribution<double> distribution(0.0, 1.0);
+std::uniform_int_distribution<unsigned long> distribution(0, 0xFFFFFFFFFFFFFFFF);
 
-static auto randomUniform = std::bind(distribution, generator);
-
-bool bernoulli(double p) {
-    return randomUniform() < p;
+unsigned long randRange(unsigned long lo, unsigned long hi) {
+    return lo + distribution(generator) % (hi - lo);
 }
 
 class Timer {
@@ -38,34 +36,27 @@ public:
 
 template <typename G>
 void largeTest(unsigned long size, double p) {
-    printf("Size: %lu, p: %f\n", size, p);
+    auto m = (unsigned long) (size * size * p);
+
+    printf("Size: %lu, m: %lu\n", size, m);
 
     Timer t;
     t.start();
-    G g;
+    G *g = G::withSize(size);
     t.stop();
     printf("Initialisation: %f seconds\n", t.read());
     t.start();
-    for (unsigned long k = 0; k < size; k++) {
-        g.insertEntry();
-    }
-    t.stop();
-    printf("Inserting entries: %f seconds\n", t.read());
-    t.start();
-    unsigned long n = 0;
-    for (unsigned long i = 0; i < size; i++) {
-//        printf("%lu\n", i);
-        for (unsigned long j = 0; j < size; j++) {
-            if (bernoulli(p)) {
-                g.addEdge(i, j);
-                n++;
-            }
-        }
+    for (unsigned long k = 0; k < m; k++) {
+        unsigned long i = randRange(0, size);
+        unsigned long j = randRange(0, size);
+        g->addEdge(i, j);
     }
     t.stop();
     printf("Adding edges: %f seconds\n", t.read());
-    printf("Size in memory: %lu bytes\n", g.memoryUsage());
-    printf("Edges: %lu\n", n);
+    printf("Size in memory: %lu bytes\n", g->memoryUsage());
+    unsigned long i = randRange(0, size);
+    unsigned long j = randRange(0, size);
+    printf("%i\n", g->reportEdge(i, j));
 }
 
 #endif // DK2TREE_LARGE_GRAPH_TEST
