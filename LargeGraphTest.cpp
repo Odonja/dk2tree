ac3e3fcd0464f6327f329ce14dd4daab474a5b09
HugoMelchers <hugomelchers@gmail.com>
2019-03-01 13:04:32 +0100

    Formatting, gave largeTest() boolean to enable/disable printing of progress

--- LargeGraphTest.cpp
+++ LargeGraphTest.cpp
@@ -10,8 +10,10 @@
 #include <functional>
 #include <thread>
 
-std::default_random_engine generator((unsigned long) std::chrono::steady_clock::now().time_since_epoch().count());
-std::uniform_int_distribution<unsigned long> distribution(0, 0xFFFFFFFFFFFFFFFF);
+std::default_random_engine generator(
+        (unsigned long) std::chrono::steady_clock::now().time_since_epoch().count());
+std::uniform_int_distribution<unsigned long> distribution(0,
+                                                          0xFFFFFFFFFFFFFFFF);
 
 unsigned long randRange(unsigned long lo, unsigned long hi) {
     return lo + distribution(generator) % (hi - lo);
@@ -34,23 +36,33 @@ public:
     }
 };
 
-template <typename G>
-G* largeTest(unsigned long size, unsigned long m) {
+template<typename G>
+G *largeTest(unsigned long size, unsigned long m, bool verbose = false) {
     Timer t;
     t.start();
     G *g = G::withSize(size);
     t.stop();
     printf("Initialisation: %f seconds\n", t.read());
-    t.start();
-    for (unsigned long k = 1; k <= m; k++) {
-        if (k % 1000 == 0) {
-            printf("%lu / %lu\r", k, m);
+    if (verbose) {
+        t.start();
+        for (unsigned long k = 1; k <= m; k++) {
+            if (k % 10000 == 0) {
+                printf("%lu / %lu\r", k, m);
+            }
+            unsigned long i = randRange(0, size);
+            unsigned long j = randRange(0, size);
+            g->addEdge(i, j);
         }
-        unsigned long i = randRange(0, size);
-        unsigned long j = randRange(0, size);
-        g->addEdge(i, j);
+        t.stop();
+    } else {
+        t.start();
+        for (unsigned long k = 1; k <= m; k++) {
+            unsigned long i = randRange(0, size);
+            unsigned long j = randRange(0, size);
+            g->addEdge(i, j);
+        }
+        t.stop();
     }
-    t.stop();
     printf("Adding edges: %f seconds\n", t.read());
     printf("Size in memory: %lu bytes\n", g->memoryUsage());
     unsigned long i = randRange(0, size);
--- main.cpp
+++ main.cpp
@@ -12,30 +12,33 @@
 #include "DKTreeTest.cpp"
 #include "TTreeTest.cpp"
 
-void doLargeTests();
+void doLargeTests(bool);
 
 int main(int argc, char **argv) {
-//    doLargeTests();
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    int k = RUN_ALL_TESTS();
+    if (k != 0) {
+        return k;
+    }
+    doLargeTests(true);
 }
 
-void doLargeTests() {
+void doLargeTests(bool verbose) {
     std::cout << ::getpid() << std::endl;
 
-    unsigned long size = 10000;
-    unsigned long m = size * 5;
+    unsigned long size = 100000;
+    unsigned long m = size * 50;
     printf("Size: %lu, m: %lu\n", size, m);
 
 //    printf("\nAdjacency matrix:\n");
-//    auto matrix = largeTest<AdjacencyMatrix>(size, m);
+//    auto matrix = largeTest<AdjacencyMatrix>(size, m, verbose);
 //    delete matrix;
 
 //    printf("\nAdjacency lists:\n");
-//    auto graph = largeTest<AdjacencyLists>(size, m);
+//    auto graph = largeTest<AdjacencyLists>(size, m, verbose);
 //    delete graph;
 
     printf("\ndk2tree:\n");
-    auto tree = largeTest<DKTree>(size, m);
+    auto tree = largeTest<DKTree>(size, m, verbose);
     delete tree;
 }
